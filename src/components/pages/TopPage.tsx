0047b50f5665b6a9df07ef178bcefbcc183ef127
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-06-15 20:19:53 +0900

    トップ画面表示

---
+++ src/components/pages/TopPage.tsx
@@ -0,0 +1,9 @@
+
+
+export default function TopPage(){
+    return(
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="font-bold text-3xl text-sky-500 underline">Next.jsスタート</h1>
+        </div>
+    )
+}

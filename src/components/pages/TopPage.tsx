633f0d148c17d8e3fcec0b90aeb87730debecbb3
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-06-15 21:20:25 +0900

    Reactに近い色にスタイリング

--- src/components/pages/TopPage.tsx
+++ src/components/pages/TopPage.tsx
@@ -5,9 +5,9 @@ export default function TopPage(){
         <div>
             <header className="leading-[60px]">
                 <div className="container mx-auto flex justify-between">
-                    <h1>スケジュール管理App</h1>
+                    <h1 className="logo">スケジュール管理App</h1>
                     <nav>
-                        <ul className="flex gap-10">
+                        <ul className="flex gap-10 text-sky-400">
                             <li>利用説明</li>
                             <li>新規登録</li>
                             <li>ログイン</li>

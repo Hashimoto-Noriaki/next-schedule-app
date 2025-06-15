4d7989e9ba0e9692e550c5198e0315ce434e8ad7
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-06-16 02:38:13 +0900

    LoginButton.tsxに型定義

--- src/components/atoms/LoginButton.tsx
+++ src/components/atoms/LoginButton.tsx
@@ -1,6 +1,4 @@
-
-
-export default function LoginButton({children}){
+export default function LoginButton({children}: { children: React.ReactNode }) {
     return(
         <button className="bg-emerald-600 text-white font-bold text-lg  px-12 py-5 w-[400px] rounded-full shadow-lg  hover:bg-emerald-400 transition">
             {children}
